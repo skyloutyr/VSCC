864f9197369cec5c7266127cedf7fb538acc736a
Sky Lou <skyloutyr@gmail.com>
2020-05-24 12:03:51 +0300

    Added range converter

---
+++ VSCC/VersionManager/SemVerRangeConverter.cs
@@ -0,0 +1,30 @@
+﻿namespace VSCC.VersionManager
+{
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+    using System;
+
+    public class SemVerRangeConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType) => objectType == typeof(string) || objectType == typeof(SemVer.Range);
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader.TokenType == JsonToken.Null)
+            {
+                return new SemVer.Range("*");
+            }
+            else
+            {
+                JToken token = JToken.Load(reader);
+                return new SemVer.Range(token.Value<string>());
+            }
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            JToken token = JToken.FromObject(((SemVer.Range)value).ToString());
+            token.WriteTo(writer);
+        }
+    }
+}
